cff828f43067882dbe33449da40bdeaef183d6d6
Harshit <harshitagarwal846@gmail.com>
2022-10-10 17:25:32 +0530

    changed identation

--- client/src/pages/items/items.page.jsx
+++ client/src/pages/items/items.page.jsx
@@ -1,17 +1,17 @@
-import { Grid } from "@mui/material";
-import Box from "@mui/material/Box";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { fetchItem } from "../../features/item/itemSlice";
-import Appetizer from "./appetizer.page";
-import Dessert from "./dessert.page";
-import Maincourse from "./maincourse.page";
-import Menu from "./menu.page";
-import Salad from "./salad.page";
-import Stacks from "./stacks.page";
-import { Timer } from "./timer.page";
-import { toast } from "react-toastify";
+import { Grid } from '@mui/material';
+import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchItem } from '../../features/item/itemSlice';
+import Appetizer from './appetizer.page';
+import Dessert from './dessert.page';
+import Maincourse from './maincourse.page';
+import Menu from './menu.page';
+import Salad from './salad.page';
+import Stacks from './stacks.page';
+import { Timer } from './timer.page';
+import { toast } from 'react-toastify';
 
 const Items = () => {
   const dispatch = useDispatch();
@@ -22,25 +22,23 @@ const Items = () => {
   const handleOption = (val) => {
     setOption(val);
   };
-  const hasData = useSelector(store => store.item.hasData);
-  const status = useSelector(store => store.user.user.status);
+  const hasData = useSelector((store) => store.item.hasData);
+  const status = useSelector((store) => store.user.user.status);
 
   useEffect(() => {
-       
-    if(hasData===false){
-        dispatch(fetchItem());
-        toast.success("hasData has run");
+    if (hasData === false) {
+      dispatch(fetchItem());
+      toast.success('hasData has run');
     }
-    
-    if(status===0){
-      navigate("/dashboard");
-      toast.error("You cannot start now");
-    }else if(status===2){
-      navigate("/dashboard");
-      toast.error("You have already purchased once");
+
+    if (status === 0) {
+      navigate('/dashboard');
+      toast.error('You cannot start now');
+    } else if (status === 2) {
+      navigate('/dashboard');
+      toast.error('You have already purchased once');
     }
-  
-  },[])
+  }, []);
   return (
     <>
       <Timer />
@@ -54,7 +52,7 @@ const Items = () => {
             {option === 4 && <Dessert handleOption={handleOption} />}
           </Grid>
           <Grid item xs={5}>
-            <Stacks/>
+            <Stacks />
           </Grid>
         </Grid>
       </Box>

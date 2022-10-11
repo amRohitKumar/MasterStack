9862f1bddef3add45696149879625d287e61887f
Uttam Pandit <uttampndt28@gmail.com>
2022-10-11 16:44:52 +0530

    Added Prizes

---
+++ client/src/pages/landingPage/components/prize.jsx
@@ -0,0 +1,64 @@
+import { Card, Grid, styled, Typography, useMediaQuery } from "@mui/material";
+import React from "react";
+
+const StyledGrid = styled(Grid)(({ theme }) => ({
+  boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
+  width: "85%",
+  borderRadius: "1rem",
+}));
+
+const Prize = (props) => {
+  const match = useMediaQuery("(min-width:600px)");
+  return (
+    <StyledGrid container spacing={0}>
+      <Grid
+        md={5}
+        sx={{
+          paddingY: "2rem",
+          paddingX: "1.4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginX: "auto",
+        }}
+      >
+        <img
+          src={props.image}
+          width={match ? "80%" : "60%"}
+          alt={props.position}
+        />
+      </Grid>
+      <Grid
+        md={7}
+        sx={{
+          padding: "0.2rem",
+          display: "flex",
+          marginX: "auto",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "2rem",
+            fontWeight: "700",
+            textAlign: "center",
+          }}
+        >
+          {props.position}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "1.3rem",
+            fontWeight: "500",
+          }}
+        >
+          Rs. {props.amount}
+        </Typography>
+      </Grid>
+    </StyledGrid>
+  );
+};
+
+export default Prize;
--- client/src/pages/landingPage/landingPage.page.jsx
+++ client/src/pages/landingPage/landingPage.page.jsx
@@ -1,23 +1,25 @@
-import { Fragment, useRef } from 'react';
-import Header from './components/Header';
-import CountDown from './components/countdown';
-import Rules from './components/rules';
-import About from './components/about';
-import MyTimeLine from './components/timeline';
-import { Footer } from '../../components';
+import { Fragment, useRef } from "react";
+import Header from "./components/Header";
+import CountDown from "./components/countdown";
+import Rules from "./components/rules";
+import About from "./components/about";
+import MyTimeLine from "./components/timeline";
+import Prizes from "./components/prizes";
+import { Footer } from "../../components";
 
 const LandingPage = () => {
   const about = useRef(null);
   const rules = useRef(null);
   const timeline = useRef(null);
-
+  const prizes = useRef(null);
   return (
     <Fragment>
-      <Header about={about} rules={rules} timeline={timeline} />
+      <Header about={about} rules={rules} timeline={timeline} prizes={prizes} />
       <CountDown />
       <About ref={about} />
       <Rules ref={rules} />
       <MyTimeLine ref={timeline} />
+      <Prizes ref={prizes} />
       <Footer />
     </Fragment>
   );
